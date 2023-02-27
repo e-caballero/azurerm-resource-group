908f520d81e79dbef5cf9dac441938a840187be5
erik caballero <erik.caballero@icloudnetwork.net>
2023-02-27 16:35:51 -0500

    updated vars

--- resource_group.tf
+++ resource_group.tf
@@ -1,7 +1,7 @@
 
 resource "azurerm_resource_group" "resource_group" {
   name     = local.resource_group_name
-  location = vars.location
+  location = var.location
   tags     = local.common_tags
 }
 
