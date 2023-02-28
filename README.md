79f5648f4093be70b4c785d162ef5b7155f09aec
erik caballero <erik.caballero@icloudnetwork.net>
2023-02-28 17:34:49 -0500

    v1

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# azure-resource-group
-azure enterprise resource group
+
+---
+
+# azurerm_resource_group
+
+Manages a Resource Group.
+
+-> **Note:** Azure automatically deletes any Resources nested within the Resource Group when a Resource Group is deleted.
+
+-> Version 2.72 and later of the Azure Provider include a Feature Toggle which can error if there are any Resources left within the Resource Group at deletion time. This Feature Toggle is disabled in 2.x but enabled by default from 3.0 onwards, and is intended to avoid the unintentional destruction of resources managed outside of Terraform (for example, provisioned by an ARM Template). See [the Features block documentation](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs#features) for more information on Feature Toggles within Terraform.
+
+## Example Usage
+
+```hcl
+resource "azurerm_resource_group" "example" {
+source = "git::https://github.com/erikcaballero/azure-resource-group?ref=v1.0"
+
+# Required Inputs
+location                  = var.location
+count_num                 = var.index
+environment               = var.environment
+application_id            = "S123456"
+component_name            = "Comp name"
+application_owner         = "example team1 manager fname lname"
+application_owner_email   = "bob@company.com"
+application_team          = "Team1"
+application_team_email    = "team1@company.com
+application_team_slack    = "https://app.slack.com/huddle/1234/123"
+application_teams_channel = "https://teams.microsoft.com/l/team/1234"
+project                   = "Sample Project"
+external                  = var.external
+cost_center               = "13245678"
+compliance                = "none"
+
+# Optional Inputs
+additional_tags           = []
+description               = "This is to provide a sample"
+}
+```
+
+
+<!-- BEGIN_TF_DOCS --> 
+
+<!-- END_TF_DOCS --> 
+
---
+++ variables.tf
@@ -0,0 +1,114 @@
+variable "location" {
+  description = "- (Required) The Azure Region where the Resource Group should exist. Changing this forces a new Resource Group to be created."
+}
+variable "additional_tags" {
+  description = "Additional tags added to all resources"
+  type        = map(any)
+  default     = {}
+}
+variable "environment" {
+  description = "The environment of the system as defined by the data classification process. Valid values for environment are (Development, Test, Production)."
+  type        = string
+  validation {
+    condition     = contains(["dev", "stg", "qa", "pi", "prd"], var.environment)
+    error_message = "Valid values for environment are (dev, stg, qa, pi, prd)."
+  }
+}
+variable "application_id" {
+  description = "The application ID of the system as defined by the data classification process. Valid values for application_id are (APP-0001, APP-0002, APP-0003)."
+  type        = string
+  validation {
+    condition     = contains(["APP-0001", "APP-0002", "APP-0003"], var.application_id)
+    error_message = "Valid values for application_id are (APP-0001, APP-0002, APP-0003)."
+  }
+}
+variable "application_owner" {
+  description = "The application owner of the system as defined by the data classification process. Valid values for application_owner are (John Doe, Jane Doe, John Smith)."
+  type        = string
+  validation {
+    condition     = contains(["John Doe", "Jane Doe", "John Smith"], var.application_owner)
+    error_message = "Valid values for application_owner are (John Doe, Jane Doe, John Smith)."
+  }
+}
+variable "application_owner_email" {
+  description = "The application owner email of the system as defined by the data classification process. Valid values for application_owner_email are ( first.last@company.com)"
+  type        = string
+  validation {
+    condition     = can(regex("^[a-z,A-Z,.]{1,}@company.com$", var.application_owner_email))
+    error_message = "Valid values for application_owner_email are (first.last@company.com)"
+  }
+}
+variable "application_team" {
+  description = "The application team of the system as defined by the data classification process. Valid values for application_team are (Team 1, Team 2, Team 3)."
+  type        = string
+  validation {
+    condition     = contains(["Team 1", "Team 2", "Team 3"], var.application_team)
+    error_message = "Valid values for application_team are (Team 1, Team 2, Team 3)."
+  }
+}
+variable "application_team_email" {
+  description = "The application team email of the system as defined by the data classification process. Valid values for application_team_email are (team1@company.com)"
+  type        = string
+}
+variable "application_team_slack" {
+  description = "The application team slack of the system as defined by the data classification process. Valid values for application_team_slack are (https://app.slack.com/huddle/1234/123)"
+  type        = string
+  validation {
+    condition     = can(regex("^(https://app.slack.com/huddle/1234/123)$", var.application_team_slack))
+    error_message = "Valid values for application_team_slack are (https://app.slack.com/huddle/1234/123)"
+  }
+  default = "https://app.slack.com/huddle/1234/123"
+}
+variable "application_teams_channel" {
+  description = "The application team Microsoft Teams channel of the system as defined by the data classification process. Valid values for application_teams_channel are (https://teams.microsoft.com/l/channel/1234/123)"
+  type        = string
+  validation {
+    condition     = can(regex("^(https://teams.microsoft.com/l/channel/1234/123)$", var.application_teams_channel))
+    error_message = "Valid values for application_teams_channel are (https://teams.microsoft.com/l/channel/1234/123)"
+  }
+  default = "https://teams.microsoft.com/l/channel/1234/123"
+}
+variable "component_name" {
+  description = "The component name of the system as defined by the data classification process. Valid values for component_name are (component1, component2, component3)."
+  type        = string
+  validation {
+    condition     = contains(["component1", "component2", "component3"], var.component_name)
+    error_message = "Valid values for component_name are (component1, component2, component3)."
+  }
+}
+
+variable "project" {
+  description = "The project of the system as defined by the data classification process. Valid values for project are (project1, project2, project3)."
+  type        = string
+}
+
+variable "description" {
+  description = "The description of the system as defined by the data classification process. Valid values for description are (description1, description2, description3)."
+  type        = string
+  default = ""
+}
+
+variable "external" {
+  description = "The external of the system as defined by the data classification process. Valid values for external are (external1, external2, external3)."
+  type        = string
+}
+
+variable "cost_center" {
+  description = "The cost center of the system as defined by the data classification process. Valid values for cost_center are (cost_center1, cost_center2, cost_center3)."
+  type        = string
+}
+
+variable "compliance" {
+  description = "The compliance of the system as defined by the data classification process. Valid values for compliance are (compliance1, compliance2, compliance3)."
+  type        = string
+}
+variable "count_number" {
+  description = "The count number of the system as defined by the data classification process. Valid values for count_number are (count_number1, count_number2, count_number3)."
+  type        = string
+}
+
+
+
+
+
+
